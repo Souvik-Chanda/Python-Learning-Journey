36672188ea7675b9502b934843d85735116f89ab
Souvik-Chanda <souvikchanda07@gmail.com>
2026-06-03 20:49:35 +0530

    Add files via upload

---
+++ Day-2/Basic_Calculator.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter the  first number :"))
+num2 = float(input("Enter the second number :"))
+add = num1+num2
+sub = num1-num2
+multi = num1*num2
+div = num1/num2
+modul = num1%num2
+# addition
+print("Addition =",add)
+# Substraction
+print("Subtraction =",sub)
+# multiplication
+print("Multiplication =",multi)
+# division
+print("Division =",div)
+# modular
+print("Modular =",modul)
