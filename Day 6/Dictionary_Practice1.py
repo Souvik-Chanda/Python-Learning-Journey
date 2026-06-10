1b95027b98ee72f95647af8f2dcfcfd50a56dc5d
Souvik-Chanda <souvikchanda07@gmail.com>
2026-06-10 23:55:09 +0530

    Add files via upload

---
+++ Day 6/Dictionary_Practice1.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "Souvik",
+    "age" : 26,
+    "city" :"Kolkata"
+
+}
+student["age"] = 27
+student["country"] = "India"
+print(student["name"])
+print(student["age"])
+print(student["city"])
+print(student["country"])
---
+++ Day 6/Dictionary_practice2.py
@@ -0,0 +1,12 @@
+book = {
+    "title" : "Python Basics",
+    "pages" : 200,
+    "author" : "John"
+}
+book["pages"] = 300
+book["price"] = 499
+print(book)
+# print(book["title"])
+# print(book["pages"])
+# print(book["author"])
+# print(book["price"])
