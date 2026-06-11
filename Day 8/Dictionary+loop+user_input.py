3e7519cc533c56fdc8f31e0daba68b236c3526dc
Souvik-Chanda <souvikchanda07@gmail.com>
2026-06-11 23:21:02 +0530

    Add files via upload

---
+++ Day 8/Dictionary+loop+user_input.py
@@ -0,0 +1,5 @@
+student = {}
+student["name"] = input("Enter name : ")
+student["city"] = input("Enter city : ")
+for key in student :
+    print(key, "=", student[key])
---
+++ Day 8/Dictionary_user_input.py
@@ -0,0 +1,5 @@
+student = {}
+student["name"] = input("Enter name : ")
+student["age"] = int(input("Enter age : "))
+student["city"] = input("Enter city : ")
+print(student)
